1eed79639bac9bfa8b7123bb918c1cfecdd44f60
shamshaev <an.shamshaev@gmail.com>
2024-10-24 14:37:38 +0300

    Initialize project

---
+++ src/main/java/shamshaev/code/PostalTrackerApplication.java
@@ -0,0 +1,13 @@
+package shamshaev.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalTrackerApplication.class, args);
+	}
+
+}
