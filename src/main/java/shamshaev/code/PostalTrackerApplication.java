bb1daf8b8dbfb7209db15aa54958dd44dfa0233a
shamshaev <an.shamshaev@gmail.com>
2024-10-31 00:11:50 +0300

    Convert tabs to spaces

--- src/main/java/shamshaev/code/PostalTrackerApplication.java
+++ src/main/java/shamshaev/code/PostalTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PostalTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostalTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostalTrackerApplication.class, args);
+    }
 
 }
--- src/test/java/shamshaev/code/PostalTrackerApplicationTests.java
+++ src/test/java/shamshaev/code/PostalTrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PostalTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
