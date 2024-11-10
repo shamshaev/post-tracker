1353e723bb88fc49d412a281483d7b33fbb4bc4c
shamshaev <an.shamshaev@gmail.com>
2024-11-11 01:00:13 +0300

    Add repository annotation

--- src/main/java/shamshaev/code/repository/PostOfficeRepository.java
+++ src/main/java/shamshaev/code/repository/PostOfficeRepository.java
@@ -1,10 +1,12 @@
 package shamshaev.code.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shamshaev.code.model.PostOffice;
 
 import java.util.Optional;
 
+@Repository
 public interface PostOfficeRepository extends JpaRepository<PostOffice, Long> {
     Optional<PostOffice> findByPostCode(String name);
 }
